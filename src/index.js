28a457428d54bb0338c1483b6507338bdfbe97b1
Aleksei Glazunov <993468@gmail.com>
2024-01-18 16:46:34 +0400

    edit mapbox key env

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  
+ReactDOM.render(  
     <App />
   ,
   document.getElementById('root')
